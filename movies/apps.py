084c042bf405ad69245ff95299c0c45c87692d4d
vinh nguyen <quangvinh.1305@gmail.com>
2024-04-28 14:46:02 +0000

    jksddsjkkjds

--- movies/apps.py
+++ movies/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
-
 class MoviesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'movies'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "movies"
+
+    def ready(self):
+        import movies.signals  # noqa
---
+++ movies/signals.py
@@ -0,0 +1,14 @@
+from django.core.signals import request_finished
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from movies.models import SearchTerm
+
+@receiver(request_finished)
+def signal_receiver(sender, **kwargs):
+    print(f"Received signal from {sender}")
+
+@receiver(post_save, sender=SearchTerm, dispatch_uid="search_term_saved")
+def search_term_saved(sender, instance, created, **kwargs):
+    if created:
+        # new SearchTerm was created
+        print(f"A new SearchTerm was created: '{instance.term}'")

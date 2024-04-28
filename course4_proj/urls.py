fba0809c148d3c0762408dd86739e55d48db6a98
xsatoshi-nakamoto <100011005+xsatoshi-nakamoto@users.noreply.github.com>
2024-04-28 17:48:22 +0700

    Update urls.py

--- course4_proj/urls.py
+++ course4_proj/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+import gh.views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", gh.views.index),
 ]
 
 if settings.DEBUG:

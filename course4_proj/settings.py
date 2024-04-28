335b39d9b72e17def971a03f288f5306218aecdf
vinh nguyen <quangvinh.1305@gmail.com>
2024-04-28 15:03:31 +0000

    dsjkdsjk

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -19,7 +19,8 @@ class Dev(Configuration):
   # Build paths inside the project like this: BASE_DIR / 'subdir'.
   BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+  EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+  ADMINS = [("Vinh", "quangvinh@example.com")]
   # Quick-start development settings - unsuitable for production
   # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
--- movies/signals.py
+++ movies/signals.py
@@ -2,6 +2,7 @@ from django.core.signals import request_finished
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from movies.models import SearchTerm
+from movies.tasks import notify_of_new_search_term
 
 @receiver(request_finished)
 def signal_receiver(sender, **kwargs):
@@ -11,4 +12,5 @@ def signal_receiver(sender, **kwargs):
 def search_term_saved(sender, instance, created, **kwargs):
     if created:
         # new SearchTerm was created
+        notify_of_new_search_term.delay(instance.term)
         print(f"A new SearchTerm was created: '{instance.term}'")
